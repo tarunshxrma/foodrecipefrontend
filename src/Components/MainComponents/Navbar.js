95b771ebfd7fef80599be96e2d67eabdb59926f0
tarunshxrma <itssharmatarun@gmail.com>
2023-07-23 13:46:57 +0530

    changes success

--- src/Components/MainComponents/Navbar.js
+++ src/Components/MainComponents/Navbar.js
@@ -10,6 +10,7 @@ import RegisterPage from "../LoginRegister/RegisterPage";
 import { GrFormClose } from 'react-icons/gr'
 import { RxCross2 } from 'react-icons/rx'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { BsChevronDown } from 'react-icons/bs'
 
 const Navbar = () => {
     const [popup, setPopup] = useState(false);
@@ -33,6 +34,36 @@ const Navbar = () => {
 
     const [isMobile, setIsMobile] = useState(false);
 
+    const [open1, setOpen1] = useState(false);
+    const [open2, setOpen2] = useState(false);
+    const [open3, setOpen3] = useState(false);
+    const [open4, setOpen4] = useState(false);
+    const [open5, setOpen5] = useState(false);
+    const [open6, setOpen6] = useState(false);
+    const [open7, setOpen7] = useState(false);
+
+    const handleOpen1 = () => {
+        setOpen1(!open1);
+    };
+    const handleOpen2 = () => {
+        setOpen2(!open2);
+    };
+    const handleOpen3 = () => {
+        setOpen3(!open3);
+    };
+    const handleOpen4 = () => {
+        setOpen4(!open4);
+    };
+    const handleOpen5 = () => {
+        setOpen5(!open5);
+    };
+    const handleOpen6 = () => {
+        setOpen6(!open6);
+    };
+    const handleOpen7 = () => {
+        setOpen7(!open7);
+    };
+
 
     return (
         <>
@@ -43,7 +74,7 @@ const Navbar = () => {
                 <button id='mobile-menu-icon' onClick={() => { setIsMobile(!isMobile) }}>
                     {isMobile ? <RxCross2 /> : <GiHamburgerMenu />}
                 </button>
-                <div id={isMobile ? "nav-links-mobile" : "navbar-recipes-categories"} onClick={() => { setIsMobile(false) }}>
+                <div id="navbar-recipes-categories">
                     <div className="navbar-recipes-categories-dropdown">
                         <ul className="navbar-recipes-categories-dropdown-button">RECIPES</ul>
                         <div className="navbar-recipes-categories-dropdown-content">
@@ -145,6 +176,204 @@ const Navbar = () => {
                         :
                         <img src={NavbarProfileIcon} alt="profile-icon" onClick={togglePopup}></img>
                     }
+                </div>
+            </div>
+            <div id={isMobile ? "dropdown2" : "navbar-recipe-categories"} className="drop">
+                <div>
+                    <button onClick={handleOpen1}>RECIPES &nbsp;<BsChevronDown /></button>
+                    {open1 ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <Link to='/recipes/breakfast and brunch' onClick={() => setIsMobile(!isMobile)}>Breakfast & Brunch Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/lunch' onClick={() => setIsMobile(!isMobile)}>Lunch Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/appetizers and snack' onClick={() => setIsMobile(!isMobile)}>Appetizers & Snack Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/dinner recipes' onClick={() => setIsMobile(!isMobile)}>Dinner Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/desert' onClick={() => setIsMobile(!isMobile)}>Desert Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/drink and cocktail' onClick={() => setIsMobile(!isMobile)}>Drink & Cocktail Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/sidedish' onClick={() => setIsMobile(!isMobile)}>Side Dish Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/grilling and bbq' onClick={() => setIsMobile(!isMobile)}>Grill & BBQ Recipes</Link>
+                            </li>
+                        </ul>
+                    ) : null}
+                </div>
+                <div>
+                    <button onClick={handleOpen2}>POPULAR &nbsp;<BsChevronDown /></button>
+                    {open2 ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <Link to='/recipes/trending now' onClick={() => setIsMobile(!isMobile)}>Trending Now</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/casserole' onClick={() => setIsMobile(!isMobile)}>Casserole Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/chilli' onClick={() => setIsMobile(!isMobile)}>Chilli Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/soup' onClick={() => setIsMobile(!isMobile)}>Soup Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/pasta' onClick={() => setIsMobile(!isMobile)}>Pasta Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/bread' onClick={() => setIsMobile(!isMobile)}>Bread Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/cookie' onClick={() => setIsMobile(!isMobile)}>Cookie Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/salad' onClick={() => setIsMobile(!isMobile)}>Salad Recipes</Link>
+                            </li>
+                        </ul>
+                    ) : null}
+                </div>
+                <div>
+                    <button onClick={handleOpen3}>MEAT AND SEAFOOD &nbsp;<BsChevronDown /></button>
+                    {open3 ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <Link to='/recipes/chicken' onClick={() => setIsMobile(!isMobile)}>Chicken Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/salmon' onClick={() => setIsMobile(!isMobile)}>Salmon recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/porkchop' onClick={() => setIsMobile(!isMobile)}>Pork Chop recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/groundbeef' onClick={() => setIsMobile(!isMobile)}>Ground Beef Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/shrimp' onClick={() => setIsMobile(!isMobile)}>Shrimp Recipes</Link>
+                            </li>
+                        </ul>
+                    ) : null}
+                </div>
+                <div>
+                    <button onClick={handleOpen4}>HEALTHY AND DIET &nbsp;<BsChevronDown /></button>
+                    {open4 ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <Link to='/recipes/keto' onClick={() => setIsMobile(!isMobile)}>Keto recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/healthy' onClick={() => setIsMobile(!isMobile)}>Healthy Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/vegetarian' v>Vegetarian Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/vegan' onClick={() => setIsMobile(!isMobile)}>Vegan Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/mediterranean' onClick={() => setIsMobile(!isMobile)}>Mediterranean Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/weightwatchers' onClick={() => setIsMobile(!isMobile)}>Weight Watchers Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/lowcarb' onClick={() => setIsMobile(!isMobile)}>Low Carb recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/glutenfree' onClick={() => setIsMobile(!isMobile)}>Gluten-Free Recipes</Link>
+                            </li>
+                        </ul>
+                    ) : null}
+                </div>
+                <div>
+                    <button onClick={handleOpen5}>HOLIDAYS &nbsp;<BsChevronDown /></button>
+                    {open5 ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <Link to='/recipes/dinner party' onClick={() => setIsMobile(!isMobile)}>Dinner Party Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/game day' onClick={() => setIsMobile(!isMobile)}>Game Day Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/valentines day' onClick={() => setIsMobile(!isMobile)}>Valentine's Day recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/st patricks day' onClick={() => setIsMobile(!isMobile)}>St. Patrick's Day Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/easter' onClick={() => setIsMobile(!isMobile)}>Easter Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/cinco de mayo' onClick={() => setIsMobile(!isMobile)}>Cinco de Mayo Day Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/mothers day' onClick={() => setIsMobile(!isMobile)}>Mother's Day Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/memorial day' onClick={() => setIsMobile(!isMobile)}>Memorial Day Recipes</Link>
+                            </li>
+                        </ul>
+                    ) : null}
+                </div>
+                <div>
+                    <button onClick={handleOpen6}>CUISINE &nbsp;<BsChevronDown /></button>
+                    {open6 ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <Link to='/recipes/mexican' onClick={() => setIsMobile(!isMobile)}>Mexican recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/italian' onClick={() => setIsMobile(!isMobile)}>Italian Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/indian' onClick={() => setIsMobile(!isMobile)}>Indian Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/thai' onClick={() => setIsMobile(!isMobile)}>Thai Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/korean' onClick={() => setIsMobile(!isMobile)}>Korean recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/french' onClick={() => setIsMobile(!isMobile)}>French rceipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/latin american' onClick={() => setIsMobile(!isMobile)}>Latin American Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/chinese' onClick={() => setIsMobile(!isMobile)}>Chinese Recipes</Link>
+                            </li>
+                        </ul>
+                    ) : null}
+                </div>
+                <div>
+                    <button onClick={handleOpen7}>SEASONAL &nbsp;<BsChevronDown /></button>
+                    {open7 ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <Link to='/recipes/spring' onClick={() => setIsMobile(!isMobile)}>Spring Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/summer' onClick={() => setIsMobile(!isMobile)}>Summer Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/fall' onClick={() => setIsMobile(!isMobile)}>Fall Recipes</Link>
+                            </li>
+                            <li className="menu-item">
+                                <Link to='/recipes/winter' onClick={() => setIsMobile(!isMobile)}>Winter Recipes</Link>
+                            </li>
+                        </ul>
+                    ) : null}
                 </div>
             </div>
 
